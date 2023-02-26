7ccd9738e46689193c912e16f01f359a69eefb48
mcorversoft <milton.corvera.ing@gmail.com>
2023-02-26 14:14:24 -0600

    Add Unit tests

--- src/test/java/com/nasa/prueba/aspirante/NasaApoloApplicationTests.java
+++ src/test/java/com/nasa/prueba/aspirante/NasaApoloApplicationTests.java
@@ -1,13 +1,28 @@
 package com.nasa.prueba.aspirante;
 
+import com.nasa.prueba.aspirante.aplicacion.restcontroller.PruebaRestController;
+import com.nasa.prueba.aspirante.dominio.services.NasaService;
+import com.nasa.prueba.aspirante.infraestructura.repository.PruebaInterfaz;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class NasaApoloApplicationTests {
 
+	@Autowired
+	private PruebaRestController restController;
+	@Autowired
+	private NasaService nasaService;
+	@Autowired
+	private PruebaInterfaz repository;
+
 	@Test
 	void contextLoads() {
+		Assertions.assertThat(restController).isNotNull();
+		Assertions.assertThat(nasaService).isNotNull();
+		Assertions.assertThat(repository).isNotNull();
 	}
 
 }
---
+++ src/test/java/com/nasa/prueba/aspirante/aplicacion/restcontroller/PruebaRestControllerTest.java
@@ -0,0 +1,25 @@
+package com.nasa.prueba.aspirante.aplicacion.restcontroller;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+class PruebaRestControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getAllNasa() {
+    }
+
+    @Test
+    void getAnSaveInfoFromNasaApi() {
+    }
+}
